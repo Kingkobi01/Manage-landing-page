a1d1dd3fac865b8e7258c7b733192bdaed7478b1
Kwabena Boakye <kwaboakye93@gmail.com>
2024-03-19 12:16:47 +0000

    Add configurations for tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                "brightRed": "#F25F3A",
+                "darkBlue": "#242D52",
+                "darkGrayishBlue": "#9095A7",
+                "veryDarkBlue": "#1D1E25",
+                "veryPaleRed": "#FFEFEB",
+                "veryLightGray": "#FAFAFA",
+            },
+            fontFamily: {
+                "vietnamPro": ["Be Vietnam Pro", "sans-serif"]
+            }
+        }
+    }
+}
