c62ebf8ec755c2aa5eb1d58e89c862a76ab00f09
Kwabena Boakye <kwaboakye93@gmail.com>
2024-04-02 21:25:05 +0000

    Update base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [],
-    base: '/vite-deploy-demo/'
+    base: '/Manage-landing-page/'
 })
